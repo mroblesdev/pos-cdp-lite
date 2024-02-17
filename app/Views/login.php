162eb2348d62bb81b414024e761b6306835e8b21
Marco Robles <78062855+mroblesdev@users.noreply.github.com>
2024-02-17 10:10:48 -0600

    Fondo para inicio de sesión Logotipo

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,14 +5,24 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="author" content="MRoblesDev" />
     <title>Sistema Punto de Venta CDP Lite v1.0</title>
+    <link rel="icon" href="<?= base_url('images/logotipo.png'); ?>" sizes="32x32" />
 
     <!-- Carga template SB Admin -->
     <link href="<?= base_url('css/styles.css'); ?>" rel="stylesheet" />
     <!-- Carga Font Awesome -->
     <link href="<?= base_url('css/all.min.css'); ?>" rel="stylesheet" />
+
+    <style>
+        body {
+            background-image: url("<?php echo base_url('images/background_pos.jpg'); ?>");
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
 
 <body class="bg-light">
@@ -24,9 +34,11 @@
                         <div class="col-lg-5">
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
                                 <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Iniciar sesión</h3>
+                                    <h3 class="text-center font-weight-light my-2"><img src="<?php echo base_url('images/logotipo.png'); ?>" width="64">&nbsp; Punto de Venta CDP Lite</h3>
+
                                 </div>
                                 <div class="card-body">
+                                    <h3 class="text-center font-weight-light mb-3">Iniciar sesión</h3>
                                     <form action="<?= base_url('login'); ?>" method="post" autocomplete="off">
                                         <?= csrf_field(); ?>
                                         <div class="form-floating mb-3">
@@ -58,11 +70,12 @@
                 </div>
             </main>
         </div>
+        <b class="text-end pe-4">Ver. Lite 1.0.0 &nbsp; </b>
         <div id="layoutAuthentication_footer">
             <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
+                <div class="container-fluid px-3">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted"><?= 'MRoblesDev © ' . date('Y'); ?></div>
+                        <div class="text-muted">POS CDP Lite - <?= date('Y'); ?> MRoblesDev</div>
                         <div>
                             <a href="https://github.com/mroblesdev/pos-cdp-lite"><i class="fa-brands fa-github"></i> GitHub</a>
                         </div>
